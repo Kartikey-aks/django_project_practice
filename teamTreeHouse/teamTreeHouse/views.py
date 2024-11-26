f61509949835f5580a6dd7af9d88f291e770d3b9
Kartikey-aks <aks.kartikey@gmail.com>
2024-11-26 23:01:12 +0530

    Created 2 folders in the root directory which are templates including html file and static folder including css and js files. Now instead of sending the httpResponse, rendering the html page at home page.

--- teamTreeHouse/teamTreeHouse/views.py
+++ teamTreeHouse/teamTreeHouse/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # methods
 def home(request):
-    return HttpResponse("You are at home page.")
+    # return HttpResponse("You are at home page.")
+    return render(request, 'index.html')
 
 def about(request):
     return HttpResponse("You are at about page.")
